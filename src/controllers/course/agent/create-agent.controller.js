4a32622c16672db460e6156defc7e66e2010fcf1
Ivan Ostapchuk <ostapchuk.ivan.2003@gmail.com>
2026-05-20 11:58:47 +0300

    update controller create agent

--- src/controllers/course/agent/create-agent.controller.js
+++ src/controllers/course/agent/create-agent.controller.js
@@ -2,7 +2,7 @@ import CourseAgent from '../../../models/course/agent.model.js';
 
 export async function createAgent(req, res) {
     try {
-        const { name, description, avatar, openAiAssistantId, baseMessages } = req.validatedData.body;
+        const { name, description, role, avatar, openAiAssistantId, baseMessages } = req.validatedData.body;
         const agent = await CourseAgent.create({ name, description, avatar, openAiAssistantId, baseMessages });
         return res.success(agent, 'Агент создан', 201);
     } catch (error) {

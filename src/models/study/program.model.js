9c4518e79670a6ebbb46313b342a281758da8cf6
Ivan Ostapchuk <ostapchuk.ivan.2003@gmail.com>
2026-06-07 19:01:45 +0300

    update data structure

--- src/models/study/program.model.js
+++ src/models/study/program.model.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const ModuleItemSchema = new mongoose.Schema({
     type: {
         type: String,
-        enum: ['lesson', 'agent'],
+        enum: ['StudyLesson', 'StudyAgent'],
         required: true
     },
     item: {
